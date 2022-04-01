8ae0a8750b83ffd9ba9814b635bf72766c503af9
bigfbb <89866348+bigfbb@users.noreply.github.com>
2022-04-01 12:02:12 +0200

    Update signup.ts
    fiexed bug that was updating the firebase

--- src/Module/signup.ts
+++ src/Module/signup.ts
@@ -1,13 +1,32 @@
 import { User } from "./User";
 import { redirector } from "../display";
+import { db } from "./firebase";
+import { ref, onValue, update, remove, push, get, child, getDatabase, DataSnapshot, } from "firebase/database";
+
+// const usrNameLogin: HTMLInputElement = document.querySelector('#user-name-login');
+
 // SignUp
 const newUsrBtn: HTMLButtonElement = document.querySelector('#new-user-button');
 const newUsrName: HTMLInputElement = document.querySelector('#user-name');
 const newPass: HTMLInputElement = document.querySelector('#new-password')
 newUsrBtn.addEventListener('click', (e: Event) => {
   e.preventDefault();
-  new User(newUsrName.value, newPass.value, (document.querySelector('#gender') as HTMLSelectElement).value, (document.querySelector('#bio') as HTMLTextAreaElement).value).sendToDb();
-  //write something to do here !!
-  // go to the main page till example
-setInterval(redirector,1500)
-})
+  const dbRef = ref(db);
+  get(child(dbRef, `/Users/`)).then((snapshot) => {
+    let userExist: boolean = false;
+    for (let usr in snapshot.val()) {
+      if (snapshot.val()[usr].userName == newUsrName.value) {
+        userExist = true;
+      }
+    }
+    if (!userExist){
+      new User(newUsrName.value, newPass.value, (document.querySelector('#gender') as HTMLSelectElement).value, (document.querySelector('#bio') as HTMLTextAreaElement).value).sendToDb();
+      alert('User successfully signed up  ')
+      setInterval(redirector,2000)
+      }
+      else{alert('user exixt')
+      }
+  })})
+
+
+
